bb423a1867ae134115c40e19cf4f694f26f7ef57
azzerilla <yazdandoost.a@gmail.com>
2020-05-11 14:33:37 -0700

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StimulCrack.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
